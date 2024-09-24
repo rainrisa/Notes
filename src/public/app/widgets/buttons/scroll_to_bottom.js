982a8659986cf1440ee78c419f98a56b3346ce31
rainrisa <risarainly@gmail.com>
2024-09-24 16:40:14 +0700

    client: Added scroll to top and bottom button

--- src/public/app/widgets/buttons/scroll_to_bottom.js
+++ src/public/app/widgets/buttons/scroll_to_bottom.js
@@ -5,13 +5,25 @@ export default class ScrollToBottomButton extends OnClickButtonWidget {
     constructor() {
         super();
 
+        this.isAtBottom = false;
+
         this.icon('bx-chevron-down')
             .title(t('onclick_button.scroll_to_bottom'))
             .class('icon-action')
             .onClick((widget, evt) => {
                 const $noteSplit = $(`.note-split[data-ntx-id="${this.noteContext.ntxId}"]`);
                 const $scrollingContainer = $noteSplit.children('.scrolling-container');
-                $scrollingContainer.scrollTop($scrollingContainer[0].scrollHeight);
+
+                if (this.isAtBottom) {
+                    $scrollingContainer.scrollTop(0);
+                    this.settings.title = t('onclick_button.scroll_to_bottom');
+                    this.$widget.addClass('bx-chevron-down').removeClass('bx-chevron-up');
+                } else {
+                    $scrollingContainer.scrollTop($scrollingContainer[0].scrollHeight);
+                    this.settings.title = t('onclick_button.scroll_to_top');
+                    this.$widget.addClass('bx-chevron-up').removeClass('bx-chevron-down');
+                }
+                this.isAtBottom = !this.isAtBottom;
             });
     }
 }
